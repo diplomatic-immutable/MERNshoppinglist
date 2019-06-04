d1488527e1e9854ce864b537bcb9bdbfda1c2526
Marcos <marcoscortorreal@gmail.com>
2019-06-04 12:32:55 -0400

    changed compose in store to composewithdevtools and imported that

--- client/src/store.js
+++ client/src/store.js
@@ -1,6 +1,7 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
+import {composeWithDevTools} from 'redux-devtools-extension/developmentOnly';
 
 const initialState = {};
 
@@ -8,7 +9,7 @@ const initialState = {};
 const store = createStore(
   rootReducer, 
   initialState, 
-  compose(
+  composeWithDevTools(
   applyMiddleware(thunk),
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 ));
