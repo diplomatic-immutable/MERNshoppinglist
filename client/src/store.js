ef5a3f69d825af7d3703574bd7426ec4a5df8d62
Marcos <marcoscortorreal@gmail.com>
2019-06-04 12:41:32 -0400

    add ternary for devtools func, compose keeps expecting a func when devtools is not installed and errors out

--- client/src/store.js
+++ client/src/store.js
@@ -11,7 +11,9 @@ const store = createStore(
   initialState, 
   composeWithDevTools(
   applyMiddleware(thunk),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  window.__REDUX_DEVTOOLS_EXTENSION__
+        ? window.__REDUX_DEVTOOLS_EXTENSION__()
+        : f => f
 ));
 
 export default store;
